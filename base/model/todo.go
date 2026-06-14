eefad002304c4406251ec9afba567ce2bf4c5b5c
asagao01F <daiki0214.fujii@icloud.com>
2026-06-14 23:12:57 +0900

    todo.goに、アカウントIDを追加

--- base/model/todo.go
+++ base/model/todo.go
@@ -5,6 +5,7 @@ import "time"
 // Todo はデータベースの todos テーブルに対応する構造体です
 type Todo struct {
 	ID          int64     `json:"id"`          // プライマリキー
+	AccountId   int64     `json:"accountId"`   // accountId
 	Title       string    `json:"title"`       // タスクのタイトル
 	Description string    `json:"description"` // タスクの詳細説明
 	Completed   bool      `json:"completed"`   // 完了フラグ
