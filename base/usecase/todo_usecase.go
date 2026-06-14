83c86f778dc51eb3f71dd754ae2c4791006d7257
asagao01F <daiki0214.fujii@icloud.com>
2026-06-14 23:14:25 +0900

    todoのusageを定義

---
+++ base/usecase/todo_usecase.go
@@ -0,0 +1,15 @@
+package usecase
+
+import (
+	"context"
+	"todo/base/model"
+)
+
+// Usecase側が「俺はこれらの機能が欲しい」とインターフェースを宣言する
+type TodoRepository interface {
+	Create(ctx context.Context, todo *model.Todo) error
+}
+
+type TodoUsecase struct {
+	repo TodoRepository // Usecaseは自分が定義したインターフェースに依存する
+}
